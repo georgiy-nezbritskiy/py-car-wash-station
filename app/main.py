3227e0e306675c7fef8460586745dded09d90dd1
Heorhii Nezbrytskyi <heorhii.nezbrytskyi@nasdaq.com>
2024-08-05 21:57:03 +0300

    fix checkstyle

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> "Car":
+    def __init__(
+            self,
+            comfort_class: int,
+            clean_mark: int, brand: str
+    ) -> "Car":
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -26,7 +30,9 @@ class CarWashStation:
 
     def calculate_washing_price(self, car: Car) -> float:
         difference = self.clean_power - car.clean_mark
-        price = car.comfort_class * difference * self.average_rating / self.distance_from_city_center
+        price = (car.comfort_class
+                 * difference
+                 * self.average_rating / self.distance_from_city_center)
         return round(price, 1)
 
     def wash_single_car(self, car: Car) -> None:
